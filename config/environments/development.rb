30befbd2eac7a23ef29a4b19230fbe27810e3c43
thomasjacobjr <thomasjacobjr@gmail.com>
2016-04-05 08:56:42 -0700

    Added Port value to email config in development env.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV["aws_ses_address"],
-    port: "",
+    port: "587",
     domain: "localhost:3000",
     user_name: ENV["aws_ses_username"],
     password: ENV["aws_ses_password"],
