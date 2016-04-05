7dddcfc2d58d9467289d84d47d03154a5fe8b3f7
thomasjacobjr <thomasjacobjr@gmail.com>
2016-04-05 11:16:30 -0700

    Added TODO - redirect to show page in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "users#show"
   devise_for :users
   resources :users, only: [:show]
+  # TODO -- Figure out how to redirect signed in users directly to show page
 end
