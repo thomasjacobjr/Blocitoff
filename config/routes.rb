dcdf097e0d586269c908711ce7858befaf3304b9
thomasjacobjr <thomasjacobjr@gmail.com>
2016-04-06 16:09:51 -0700

    Generate Item Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  root "users#show"
-
   devise_for :users
 
   resources :users, only: [:show]
 
-  # TODO -- Figure out how to redirect signed in users directly to show page
+  root "users#show"
+
 end
