dcca9109b700cd2a7aea35fae819842ade0b61e6
thomasjacobjr <thomasjacobjr@gmail.com>
2016-04-06 16:16:07 -0700

    Add create action only to item resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show] do
-    resources :items
+    resources :items, only: [:create]
   end
 
   root "users#show"
