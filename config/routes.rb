0a29fef9c84ed53a2863367a9f8708af30dcb4db
thomasjacobjr <thomasjacobjr@gmail.com>
2016-04-05 19:51:27 -0700

    User gets redirected to profile page after signing in
    - added after_sign_in_path_for method to app controller
    -

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root "users#show"
+
+  root "welcome#index"
+
   devise_for :users
+
   resources :users, only: [:show]
+
   # TODO -- Figure out how to redirect signed in users directly to show page
 end
