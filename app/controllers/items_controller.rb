1e072b44a42deb04bd0fc36f664f3bd34badf3f0
thomasjacobjr <thomasjacobjr@gmail.com>
2016-04-08 08:58:43 -0700

    Users can see To-Do List on Own Profile Page
    - Flash messages for success/failure added to application layout
    - Fixed render partial for item list
    - Validates presence of name of item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,19 +2,21 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @item.user = current_user
 
     if @item.save
       flash[:notice] = "To-Do Item Saved"
-      redirect_to root_path 
+      redirect_to root_path
     else
-      flash.now[:alert] = "There was error saving your To-Do Item, please try again."
+      flash[:alert] = "There was error saving your To-Do Item, please try again."
+      redirect_to root_path
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:id, :name, :user_id)
+    params.require(:item).permit(:name)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @item = Item.new
+    @items = Item.where(user_id: current_user.id)
   end
 
 end
